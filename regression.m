cb744b84197a5aed8a20e9833dc98e3f20d83482
Kristian Hartikainen <kristian@statwing.com>
2015-10-15 19:14:40 +0300

    regression: Removed the useless leave one out validation.

--- regression.m
+++ regression.m
@@ -12,48 +12,26 @@ training_data = table2array(training_data_table(:,1:12));
 test_data = table2array(test_data_table(:,1:12));
 
 N_train = size(training_data, 1);
-
-dim_data = 11;
+data_dim = size(training_data, 2) - 1;
 
 perm = randperm(N_train);
 
 % we are substituting the different degrees with different dimensions of
 % linear regressors
-basis_funs = ones(N_train, (11+1));
-for i = 1:dim_data
-    basis_funs(:, (i+1) ) = training_data(:,i);
-end
+basis_funs = ones(N_train, (data_dim+1));
+basis_funs(:, 2:end) = training_data(:, 1:data_dim);
 
 valid_preds = zeros(N_train,1);
 
-for i = 1:size(training_data,1)
-    % learn model in training set
-    training_idx = perm([1:i-1 i+1:end]);
-    test_idx = perm(i);
-
-    X_tmp = basis_funs(training_idx, 1:11);
-    betas_tmp = pinv(X_tmp) * training_data(training_idx,12);
-    valid_preds(test_idx) = basis_funs(test_idx, 1:11) * betas_tmp;
-    train_preds = basis_funs(:, 1:11) * betas_tmp;
-end
-
-% compute training errors
-training_errs = train_preds - training_data(:,12);
-
-% compute validation errors
-valid_errs = valid_preds - training_data(:,12);
+betas_tmp = basis_funs \ training_data(:, 12);
 
 % test data
 n_test_data = size(test_data,1);
 
-basis_funs_test_data = ones(n_test_data, (11 + 1) );
+basis_funs_test = ones(n_test_data, data_dim + 1);
+basis_funs_test(:, 2:end) = test_data(:, 2:data_dim+1);
 
-for i = 1:dim_data
-    % test_data has id as the first column
-    basis_funs_test_data(:, (i+1) ) = test_data(:,i+1);
-end
-
-test_preds =  round(basis_funs_test_data(:, 1:11) * betas_tmp);
+test_preds =  round(basis_funs_test * betas_tmp);
 
 f = fopen('output','w');
 fprintf(f,'id,quality\n');
